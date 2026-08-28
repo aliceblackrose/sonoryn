06190809411f5da78ef94f2086e2ecbb19226826
aliceblackrose <79605349+aliceblackrose@users.noreply.github.com>
2026-08-28 01:47:40 -0500

    refactor: keep app state internal to the bot crate

--- src/state.rs
+++ src/state.rs
@@ -6,14 +6,14 @@ use tokio::sync::{RwLock, mpsc};
 use crate::gateway_control::GatewayControl;
 
 #[derive(Clone)]
-pub struct AppState {
-    pub cache: Arc<RwLock<Cache>>,
-    pub gateway_control: mpsc::Sender<GatewayControl>,
+pub(crate) struct AppState {
+    pub(crate) cache: Arc<RwLock<Cache>>,
+    pub(crate) gateway_control: mpsc::Sender<GatewayControl>,
 }
 
 impl AppState {
     #[must_use]
-    pub fn new(gateway_control: mpsc::Sender<GatewayControl>) -> Self {
+    pub(crate) fn new(gateway_control: mpsc::Sender<GatewayControl>) -> Self {
         Self {
             cache: Arc::new(RwLock::new(Cache::new(CacheConfig::new()))),
             gateway_control,
