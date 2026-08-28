b940cdd60bf5470f7aa6ce5ec5bf9b35b90d87f5
aliceblackrose <79605349+aliceblackrose@users.noreply.github.com>
2026-08-28 01:44:22 -0500

    feat: compose voice control slash commands

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,3 +1,10 @@
 mod diagnostics;
+mod voice_control;
 
-pub(crate) use diagnostics::command_list;
+use crate::state::AppState;
+
+pub(crate) fn command_list() -> Vec<gloam_commands::SlashCommand<AppState>> {
+    let mut commands = diagnostics::command_list();
+    commands.extend(voice_control::command_list());
+    commands
+}
