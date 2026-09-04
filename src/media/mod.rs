18a4ea0a3d652ff6dab7ce9ef0e0297ecdbb46cc
aliceblackrose <79605349+aliceblackrose@users.noreply.github.com>
2026-09-04 06:29:05 -0500

    feat: export playlist resolver cap

--- src/media/mod.rs
+++ src/media/mod.rs
@@ -6,7 +6,7 @@ mod ytdlp;
 pub use decoder::{
     DecodeError, EncodedOpusFrame, FfmpegDecodeOptions, FfmpegOpusDecoder, FfmpegOpusStream,
 };
-pub use resolver::{ResolveError, ResolveFuture, TrackResolver};
+pub use resolver::{MAX_PLAYLIST_ITEMS, ResolveError, ResolveFuture, TrackResolver};
 pub use track::{
     PlayableMedia, RequestedBy, ResolvedTrack, Track, TrackId, TrackMetadata, TrackRequest,
     TrackSource,
