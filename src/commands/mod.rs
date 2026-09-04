c9cfcb02a222c18214a130677155e2816b1f0ea2
aliceblackrose <79605349+aliceblackrose@users.noreply.github.com>
2026-09-04 06:17:41 -0500

    feat: register advanced playback controls

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,5 +1,6 @@
 mod diagnostics;
 mod playback;
+mod playback_controls;
 mod queue;
 mod voice_control;
 
@@ -8,6 +9,7 @@ use crate::state::AppState;
 pub(crate) fn command_list() -> Vec<gloam_commands::SlashCommand<AppState>> {
     let mut commands = diagnostics::command_list();
     commands.extend(playback::command_list());
+    commands.extend(playback_controls::command_list());
     commands.extend(queue::command_list());
     commands.extend(voice_control::command_list());
     commands
